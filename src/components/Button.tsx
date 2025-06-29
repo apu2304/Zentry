cfc65c3d160c6451460f5411f9ab6a7c2e58d673
apu2304 <paulsarpita@gmail.com>
2025-06-29 18:47:57 +0530

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+
+type ButtonProps = {
+    id: string;
+    title: string;
+    leftIcon?: React.ReactElement;
+    containerClass?: string;
+    rightIcon?: React.ReactElement;
+}
+const Button = ({id, title, leftIcon, rightIcon, containerClass}: ButtonProps) => {
+  return (
+   <button id={id} className={`group relative z-10 w-fit cursor-pointer 
+    overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`} >
+     {leftIcon}
+     <span className="relative inline-flex overflow-hidden
+     font-general text-xs uppercase">
+      <div>
+        {title}
+      </div>
+     </span>
+     {rightIcon}
+   </button>
+  )
+}
+
+export default Button
